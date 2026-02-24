9e416e2cebaa7b79131284bac2a8b93498c14003
ADITYA BHARDWAJ <adityabhardwajpcm@gmail.com>
2026-02-24 17:06:33 +0530

    Add files via upload

---
+++ Codes/Q8_StudentScoreCalculator.cpp
@@ -0,0 +1,78 @@
+/* 
+Anna is a contender for valedictorian of her high school. She wants to know 
+how many students (if any) have scored higher than her in the exams given 
+during this semester. 
+
+Create a class named Student with the following specifications: 
+- An instance variable named scores holds a student's 5 exam scores. 
+- A void input() function reads 5 integers and saves them to scores. 
+- An int calculateTotalScore() function that returns the sum of the student's scores.
+
+Name   - ADITYA BHARDWAJ
+Section- D2
+Roll No- 08
+Course – B TECH
+Branch – CSE
+*/
+
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    int scores[5];  
+
+public:
+
+    void input() {
+        for (int i = 0; i < 5; i++) {
+            cin >> scores[i];
+        }
+    }
+
+ 
+    int calculateTotalScore() {
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += scores[i];
+        }
+        return sum;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;  
+
+    Student students[100];  
+
+    for (int i = 0; i < n; i++) {
+        students[i].input();
+    }
+
+
+    int annaScore = students[0].calculateTotalScore();
+
+
+    int count = 0;
+    for (int i = 1; i < n; i++) {
+        if (students[i].calculateTotalScore() > annaScore) {
+            count++;
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
+
+/*
+Sample Input:
+3
+30 40 45 10 10
+40 40 40 10 10
+50 20 30 10 10
+
+Sample Output:
+1
+*/
